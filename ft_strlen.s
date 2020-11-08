df8267a5833d68984f6ab4a85d1ae97bd3398b70
kukim <kukim@student.42seoul.kr>
2020-11-08 17:10:37 +0900

    Add : asm strlen

---
+++ ft_strlen.s
@@ -0,0 +1,26 @@
+
+section .text
+	global _ft_strlen
+
+_ft_strlen:
+	mov rax, 0
+	jmp loopcount
+
+loopcount:			
+	cmp BYTE[rdi + rax], 0		; if 동일 cmp 비교
+	je	exit					; cmp와 비교한 값이 0이라면 exit로 가라
+	inc rax						; rax++
+	jmp loopcount				; lencount loop
+
+exit:
+	ret							; ft_strlen()함수 호출한 곳 아래 코드로 내려감
+
+; char	ft_strlen(const char *str)
+; {
+; 	int		len;
+; 
+; 	len = 0;
+; 	while (*src++)
+; 		len++;
+; 	return (len);
+; }
